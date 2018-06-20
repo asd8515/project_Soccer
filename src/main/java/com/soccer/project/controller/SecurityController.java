889445a4ff7a1e027430e878ebd5d16f0f1e0750
student <student@M15016>
2018-06-20 10:30:28 +0900

    login

--- src/main/java/com/soccer/project/controller/SecurityController.java
+++ src/main/java/com/soccer/project/controller/SecurityController.java
@@ -7,19 +7,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class SecurityController {
 
-
-	@RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
 	public void login(Locale locale, Model model) {
 		model.getClass();
 	}
-/*
-	@RequestMapping(value = {"/j_spring_security_logout"}, method = RequestMethod.GET)
+
+	@RequestMapping(value = { "/j_spring_security_logout" }, method = RequestMethod.GET)
 	public String logout(Locale locale, Model model) {
 		return "home";
 	}
-*/
+
 }
