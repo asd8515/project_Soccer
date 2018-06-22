e13307b71db8fbf1f5b9b224f71c878142085e77
clap <mr.clap8@gmail.com>
2018-06-22 11:06:52 +0900

    member/ email, name 검색 대소문자 구분없이 일부만으로 검색 기능 추가

--- DB/commonQuery.sql
+++ DB/commonQuery.sql
@@ -5,7 +5,7 @@ select * from AUTHORITY;
 select * from AUTHORITY_REL;
 
 --member table
-SELECT * FROM MEMBER
+SELECT * FROM MEMBER;
 
 --member list inner join with AUTHORITY
 select 
@@ -16,4 +16,11 @@ select
 	, AUTH.AUTHORITY as AUTHORITY
 from MEMBER MEM INNER JOIN AUTHORITY_REL AUTH_REL
 			  			ON MEM.MEMBER_SEQ = AUTH_REL.MEMBER_SEQ INNER JOIN AUTHORITY AUTH
-			  															ON AUTH_REL.ID_AUTHORITY = AUTH.ID_AUTHORITY; 
+			  															ON AUTH_REL.ID_AUTHORITY = AUTH.ID_AUTHORITY;
+			  															
+			  															
+-- to assign authority into some member
+INSERT INTO AUTHORITY_REL (MEMBER_SEQ, ID_AUTHORITY) VALUES('UUID-1541', 'UUID-A001');
+INSERT INTO AUTHORITY_REL (MEMBER_SEQ, ID_AUTHORITY) VALUES('UUID-7845887', 'UUID-A001');
+
+
